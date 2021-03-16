098bd16bc9ea30aed08613d05e76626a52e85336
Liz P <parisielizabeth@gmail.com>
2021-03-16 15:17:24 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -4,42 +4,39 @@ import { connect } from 'react-redux';
 class CreateTodo extends Component {
   
   state = {
-      text: '',
+    text: ''
   };
-  
+ 
   handleChange = event => {
     this.setState({
       text: event.target.value
     });
   };
-  
+ 
   handleSubmit = event => {
     event.preventDefault();
-    this.props.addTodo(this.state);
+    this.props.dispatch({ type: 'ADD_TODO', payload: this.state });
   };
-  
-  render(){
-  return(
-    <div>
-      <form onSubmit={event => this.handleSubmit(event)}>
-        <p>
-          <label>add todo</label>
-          <input type="text" onChange={event => this.handleChange(event)} value={this.state.text}/>
-        </p>
-        <input type="submit" />
-      </form>
-      {this.state.text}
-    </div>
+ 
+  render() {
+    return (
+      <div>
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <p>
+            <label>add todo</label>
+            <input
+              type="text"
+              onChange={event => this.handleChange(event)}
+              value={this.state.text}
+            />
+          </p>
+          <input type="submit" />
+        </form>
+      </div>
     );
   }
-  
-  const mapDispatchToProps = dispatch => {
-    return {
-      addToDo: formData => dispatch({
-        type: 'ADD_TODO', payload: formData
-      })
-    };
-  };
 }
+ 
+export default connect()(CreateTodo);
 
 export default connect(null, mapDispatchToProps)(CreateTodo);
