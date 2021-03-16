91fcd34d61d328682863b68c1cf86f1989b2a2f6
Liz P <parisielizabeth@gmail.com>
2021-03-16 14:56:58 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -22,10 +22,11 @@ class CreateTodo extends Component {
       <form>
         <p>
           <label>add todo</label>
-          <input type="text" onChange={(event) => this.handleChange(event)}/>
+          <input type="text" onChange={this.handleChange} value={this.state.text}/>
         </p>
         <input type="submit" />
       </form>
+      {this.state.text}
     </div>
     );
   }
