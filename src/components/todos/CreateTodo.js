25bd9dc26c5f38437bcbf2d0189415b9d80d9f7b
Liz P <parisielizabeth@gmail.com>
2021-03-16 15:10:03 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -17,6 +17,11 @@ class CreateTodo extends Component {
     });
   };
   
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state)
+  }
+  
   render(){
   return(
     <div>
